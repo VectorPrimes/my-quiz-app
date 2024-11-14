476560eb44c5c707b401a6dad37514cd8cc42bb5
VectorPrimes <shegsteam@gmail.com>
2024-11-14 16:23:09 +0100

    first commit

--- src/Quiz App/Pages/Result.js
+++ src/Quiz App/Pages/Result.js
@@ -15,7 +15,7 @@ function Result(){
             <Confetti />
             <h3>Congratulations {userName}! You got {result.answeredCorrectly} out of 30 questions</h3>
             <h3 style={gradeStyle}>{Math.floor((result.answeredCorrectly / 30) * 100)}%</h3>
-            <p>You can scroll below to see the corrections. (No Explanation Given! Just the correct answers).</p>
+            <p>You can scroll below to see the made corrections. (No Explanation Given! Just the correct answers).</p>
             <p>Thanks again for helping me test my application.</p>
         </div>
     )
